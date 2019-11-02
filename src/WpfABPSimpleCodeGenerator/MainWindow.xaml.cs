ba9d03ac0143b357bc9d7cbd272700258dc9f393
莫宏智 <mohongzhi1998@gmail.com>
2019-11-02 09:09:07 +0800

    languages added

--- src/WpfABPSimpleCodeGenerator/MainWindow.xaml.cs
+++ src/WpfABPSimpleCodeGenerator/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,27 @@ namespace WpfABPSimpleCodeGenerator
         public MainWindow()
         {
             InitializeComponent();
+
+            #region Load languages from the resources
+            CultureInfo Culture = CultureInfo.CurrentCulture;
+            List<ResourceDictionary> dictionaryList = new List<ResourceDictionary>();
+            foreach (ResourceDictionary dictionary in Application.Current.Resources.MergedDictionaries)
+            {
+                dictionaryList.Add(dictionary);
+            }
+            string requestedCulture = string.Format(@"Resources\StringResource.{0}.xaml", Culture);
+            ResourceDictionary resourceDictionary = dictionaryList.FirstOrDefault(d => d.Source.OriginalString.Equals(requestedCulture));
+            if (resourceDictionary == null)
+            {
+                requestedCulture = @"Resources\StringResource.xaml";
+                resourceDictionary = dictionaryList.FirstOrDefault(d => d.Source.OriginalString.Equals(requestedCulture));
+            }
+            if (resourceDictionary != null)
+            {
+                Application.Current.Resources.MergedDictionaries.Remove(resourceDictionary);
+                Application.Current.Resources.MergedDictionaries.Add(resourceDictionary);
+            } 
+            #endregion
         }
     }
 }
