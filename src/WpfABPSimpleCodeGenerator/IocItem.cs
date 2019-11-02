c6fce8eb5416dbe478045c2d174d3e658979fdf0
Hong <mohongzhi1998@gmail.com>
2019-11-02 09:42:10 +0800

    added

---
+++ src/WpfABPSimpleCodeGenerator/IocItem.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WpfABPSimpleCodeGenerator
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class IocItem
+    {
+        /// <summary>
+        /// to binding name
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// remark
+        /// </summary>
+        public string Summary { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string ConstructionName { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string AttributeName { get; set; }
+
+    }
+}
